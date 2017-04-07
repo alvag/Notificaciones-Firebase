b2eafeaabb3bef4e10d239433fcb75fe62d3636e
Max Alva <alva85@gmail.com>
2017-04-06 23:51:14 -0500

    Prueba de envio de notificaciones
    Prueba de envio de notificaciones con Firebase a todos los usuario y usuarios individuales

--- app/src/main/java/com/maxalva/notificacionesfirebase/MainActivity.java
+++ app/src/main/java/com/maxalva/notificacionesfirebase/MainActivity.java
@@ -2,12 +2,31 @@ package com.maxalva.notificacionesfirebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.iid.FirebaseInstanceId;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnToken;
+    TextView textViewToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewToken = (TextView) findViewById(R.id.textViewToken);
+        btnToken = (Button) findViewById(R.id.btnToken);
+        btnToken.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tokenFirebase = FirebaseInstanceId.getInstance().getToken();
+                textViewToken.setText(tokenFirebase);
+                System.out.println("tokenFirebase ==> " + tokenFirebase);
+            }
+        });
     }
 }
